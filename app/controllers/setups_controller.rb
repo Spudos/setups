71666a840764a6e927d923f2fb924e6680555e7c
Andy Pierce <spudos16@gmail.com>
2023-04-08 23:58:46 +0100

    update actions added

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -9,13 +9,31 @@ class SetupsController < ApplicationController
     end
 
     def new
+        @setup = Setup.new
+    end
 
+    def edit
+      @setup = Setup.find(params[:id])
     end
 
     def create
       @setup = Setup.new(params.require(:setup).permit(:car, :driver, :track))
-      @setup.save
-      redirect_to @setup
+      if @setup.save
+        flash[:notice] = "Setup was created successfully."
+        redirect_to @setup 
+      else
+        render(:new, status: :unprocessable_entity) 
+      end    
+    end
+
+    def update
+        @setup = Setup.find(params[:id])
+        if @setup.update(params.require(:setup).permit(:car, :driver, :track))
+          flash[:notice] = "Setup was edited successfully."
+          redirect_to @setup 
+        else
+          render(:edit, status: :unprocessable_entity)
+        end
     end
 
 end

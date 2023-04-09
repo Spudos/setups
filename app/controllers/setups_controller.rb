316e55d105eae7829395d26d8e6ff640cd7996db
Andy Pierce <spudos16@gmail.com>
2023-04-09 08:54:59 +0100

    dry code added

--- app/controllers/setups_controller.rb
+++ app/controllers/setups_controller.rb
@@ -1,7 +1,7 @@
 class SetupsController < ApplicationController
+before_action :set_setup, only: [:show, :edit, :update, :destroy]
 
     def show
-      @setup = Setup.find(params[:id])
     end
 
     def index
@@ -9,15 +9,14 @@ class SetupsController < ApplicationController
     end
 
     def new
-        @setup = Setup.new
+      @setup = Setup.new
     end
 
     def edit
-      @setup = Setup.find(params[:id])
     end
 
     def create
-      @setup = Setup.new(params.require(:setup).permit(:car, :driver, :track))
+      @setup = Setup.new(setup_params)
       if @setup.save
         flash[:notice] = "Setup was created successfully."
         redirect_to @setup 
@@ -27,8 +26,7 @@ class SetupsController < ApplicationController
     end
 
     def update
-        @setup = Setup.find(params[:id])
-        if @setup.update(params.require(:setup).permit(:car, :driver, :track))
+        if @setup.update(setup_params)
           flash[:notice] = "Setup was edited successfully."
           redirect_to @setup 
         else
@@ -37,9 +35,17 @@ class SetupsController < ApplicationController
     end
 
     def destroy
-      @setup = Setup.find(params[:id])
       @setup.destroy
       redirect_to setups_path
     end
 
+    private
+
+    def set_setup
+      @setup = Setup.find(params[:id])
+    end
+
+    def setup_params
+      (params.require(:setup).permit(:car, :driver, :track))
+    end
 end
